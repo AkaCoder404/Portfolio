cfa86695b538c5d260ce53e84eb49f9917feb371
akacoder404 <akacoder404@outlook.com>
2023-03-28 12:06:51 +0800

    update features

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'George Li is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://akacoder404.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/demo.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
   plugins: [
